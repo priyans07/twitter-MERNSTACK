b6c70e288a3d466fe0e23f88f9893e1ee49f5675
priyanshu Srivastava <priyanshusrivastava@policybazaar.com>
2025-03-07 21:26:33 +0530

    completed left side bar design

--- twitter clone/frontend/twitter_clone/src/component/LeftSideBar.jsx
+++ twitter clone/frontend/twitter_clone/src/component/LeftSideBar.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { CiHome } from "react-icons/ci";
+import { CiHashtag } from 'react-icons/ci';
+import { CiBookmark } from 'react-icons/ci';
+import { IoIosNotificationsOutline } from 'react-icons/io';
+import { CiUser } from 'react-icons/ci';
+import { AiOutlineLogout } from 'react-icons/ai';
 const LeftSideBar = () => {
   return (
    <div>
@@ -13,22 +18,22 @@ const LeftSideBar = () => {
       <div className='font-bold text-lg ml-2'>Home</div>
     </div>
     <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer'>
-      <CiHome size={25}/>
+      <CiHashtag size={25}/>
       <div className='font-bold text-lg ml-2'>Explore</div>
     </div>
     <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer'>
-      <CiHome size={25}/>
+      <IoIosNotificationsOutline size={25}/>
       <div className='font-bold text-lg ml-2'>Notification</div>
     </div>
     <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer'>
-      <CiHome size={25}/>
+      <CiBookmark size={25}/>
       <div className='font-bold text-lg ml-2'>Bookmark</div>
     </div>
     <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer'>
-      <CiHome size={25}/>
+      <AiOutlineLogout size={25}/>
       <div className='font-bold text-lg ml-2'>Logout</div>
     </div>
-    <button className='border'>Post</button>
+    <button className='border-none px-4 py-2 text-md bg-[#1D9BF0] rounded-full w-full text-white font-bold'>Post</button>
     </div>
    </div>
   )
